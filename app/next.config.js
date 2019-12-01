c848c5246c1c90196259931479620c0458df74aa
mevepe <udestroyed@gmail.com>
2019-12-01 16:02:35 +0300

    next config ...

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,10 @@
 const withSass = require('@zeit/next-sass')
+const { distDir } = require('../config');
+
 module.exports = withSass({
+    distDir: `../${distDir}/www`,
+    env: {
+      USER_HAS_PORTFOLIO: !!process.env.IFRAMELY_API_KEY,
+    },
     /* config options here */
 })
